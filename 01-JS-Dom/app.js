8343a914545280bc96c31ea58b2e8e0bba4020b5
Anurag <anuragsharma921998@gmail.com>
2025-05-19 12:03:19 +0530

    Basic-of-dom-selecting-elements

--- 01-JS-Dom/app.js
+++ 01-JS-Dom/app.js
@@ -1,11 +1,31 @@
-console.log(window);
-console.dir(document);
-console.dir(document.all);
+// console.log(window);
+// console.log(document);
+// console.dir(document.all);
 
-//access h1 at index 8.
-console.dir(document.all[8]);
-console.dir(document.all[8].innerHTML);
+// //access h1 at index 8.
+// console.dir(document.all[8]);
+// console.dir(document.all[8].innerHTML);
 
-//change the value of h1
-document.all[8].innerHTML = "peter parker";
-console.dir(document.all[8].innerHTML);
+// //change the value of h1
+// document.all[8].innerHTML = "Peter parker";
+// console.dir(document.all[8].innerHTML);
+
+// //SELECTING ELEMENTS.
+
+// //Elementby ID.
+// console.log(document.getElementById("footer"));
+// //Let store into variable (it's a object).
+// let foot = document.getElementById("footer");
+// console.log(foot);
+// console.dir(foot);
+// console.dir(foot.tagName);
+
+//Elementby ClassName.
+console.log(document.getElementsByClassName("project-card"));
+console.log(document.getElementsByClassName("project-card")[0].tagName);
+
+let card = document.getElementsByClassName("project-card");
+
+for(let i = 0; i <= card.length; i++){
+    console.log(card);
+}
