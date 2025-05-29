e562dc8e2806ea707086b631fbd938a75038f52b
Anurag <anuragsharma921998@gmail.com>
2025-05-29 10:34:53 +0530

    DOM-events

--- 02-JS-DOM/keyboard.js
+++ 02-JS-DOM/keyboard.js
@@ -18,9 +18,55 @@
 // })
 
 //      FORM EVENT
+// let form = document.querySelector("form");
+
+// form.addEventListener("submit", function(event) {
+//     event.preventDefault();
+//     alert("Form submitted");
+// })
+
+
+//          EXTRACTING FORM DATA.
+// let form = document.querySelector("form");
+
+// form.addEventListener("submit", function(event) {
+//     event.preventDefault();
+
+//     // console.dir(form);
+//     // console.dir(form.elements[0]);
+//     // console.dir(form.elements[1]);
+//     // console.dir(form.elements[2]);
+
+//     // let user = document.querySelector("#user");
+//     // let pass = document.querySelector("#password");
+
+//     let user = this.elements[0];
+//     let pass = this.elements[1];
+
+//     console.log(user.value);
+//     console.log(pass.value);
+
+
+
+//     alert(`Hi ${user.value}, your password is set to ${pass.value}`);
+// })
+
+
+//            MORE EVENTS.
 let form = document.querySelector("form");
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
-    alert("Form submitted");
-})
+});
+
+let user = document.querySelector("#user");
+
+user.addEventListener("change", function() {
+    console.log("changed event");
+    console.log("final value : ", this.value);
+});
+
+user.addEventListener("input", function() {
+    console.log("Input changed");
+    console.log("final value : ", this.value);
+});
